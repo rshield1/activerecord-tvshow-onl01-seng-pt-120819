dbce9d328a0ffee3e2c04459c05e5de386f08ca9
Robert Shields <robert.shieldsiv@gmail.com>
2020-03-08 04:49:02 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,7 +3,13 @@ class Show < ActiveRecord::Base
   
   def self.highest_rating
       self.maximum(:rating)
-    end
+  end
+  
+  def self.most_popular_show
+      self.where("rating = ?", self.highest_rating).first
+  end
+    
+    
   
   
   
