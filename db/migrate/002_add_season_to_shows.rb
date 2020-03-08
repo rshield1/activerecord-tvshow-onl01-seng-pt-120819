9d8807d20ff1bf9a3c32cc3490436f08d09e3d6d
Robert Shields <robert.shieldsiv@gmail.com>
2020-03-08 04:45:13 +0000

    Automatically backed up by Learn

---
+++ db/migrate/002_add_season_to_shows.rb
@@ -0,0 +1,14 @@
+class AddShow < ActiveRecord::Migration[5.2]
+  
+  def change
+    
+    create_table :shows do |t|
+      t.string :name
+      t.string :network
+      t.string :day
+      t.integer :rating 
+    end
+    
+  end
+  
+end
